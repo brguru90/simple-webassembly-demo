a3e450d2cf4034cb050c0b2336f1ddc6a70fd233
Guruprasad BR <brguru90@gmail.com>
2020-07-20 21:35:55 +0530

    added some description in cpp

--- pgm.cpp
+++ pgm.cpp
@@ -2,9 +2,14 @@
 #include <string.h>
 #include <cstdint>
 
-extern "C" void jsPrintString(const char *s, uint16_t len);
+
+// & jsPrintString is defined in js & used as calback so we need to specify it as extern
+// Like c the name will not be kept as same so we need to specify it as extern "C so we can call these function like normal c function"
+
+
 extern "C"
 {
+   void jsPrintString(const char *s, uint16_t len);
    
    char *c_hello()
    {
